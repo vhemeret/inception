bab8ddd1932f0d451541725a984018bf675e67dc
Chino <70980882+vhemeret@users.noreply.github.com>
2023-06-19 13:28:22 +0200

    some things need to be checked

---
+++ srcs/requirements/mariadb/bdd.sh
@@ -0,0 +1,13 @@
+service mysql start;
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+
+# REBOOT Mysql
+mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+
+# SWITCH TO SAFE MODE RECOMMENDED BY MYSQL
+exec mysqld_safe
